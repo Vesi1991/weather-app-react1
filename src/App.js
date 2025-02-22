a86a6ed4db0bb663f8acc2c767ac0fdb11ad2d0d
Vesi1991 <vesela-offer@web.de>
2025-02-22 09:55:22 +0100

    Added the gitHub Url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import "./App.css";
 import React from "react";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by Vesela Offer and is{" "}
+        <a
+          href="https://github.com/Vesi1991/weather-app-react1"
+          terget="_blank"
+        >
+          open- sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
