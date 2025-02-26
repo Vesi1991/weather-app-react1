66f158d18dfe7ef64920b85269f7bc3e9bca291b
Vesi1991 <vesela-offer@web.de>
2025-02-26 10:32:35 +0100

    use the correct icon url

--- src/Weather.js
+++ src/Weather.js
@@ -15,8 +15,8 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
       city: response.data.name,
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
     });
   }
 
